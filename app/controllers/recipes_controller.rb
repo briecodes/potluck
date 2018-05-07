fd8ca81af24a144f955ada254a8d4383994de404
Daniel Brazel <dbrazel4@gmail.com>
2018-05-07 17:46:35 -0400

    Made form partial for recipes, added methods to the controller and added some views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+
+  end
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :details, :user_id)
+  end
 end
