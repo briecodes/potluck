bfd19deb0d7124f82ca572b898dcd50d9ed48622
github username <jrgreiner115@gmail.com>
2018-05-08 17:33:23 -0400

    added sessions to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
 
     if @user.valid?
+      session[:user_id] = @user.id
       redirect_to @user
     else
       flash[:errors] = @user.errors.full_messages
