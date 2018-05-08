34396e5da001042896dfcc3737a6b700c84d27d8
briecodes <hello@brie.work>
2018-05-08 12:08:04 -0400

    Ingredient seed file created, seeds.rb defined for ingredient seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,15 +7,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.destroy_all
-Ingredient.destroy_all
-User.destroy_all
-Recipe.destroy_all
+csv_text = File.read(Rails.root.join('lib', 'assets', 'foods2.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Ingredient.new
+  t.name = row
+  t.save
+end
+
+# Event.destroy_all
+# User.destroy_all
+# Recipe.destroy_all
 
 Event.create(title: "Mod2 Potluck", location: "Kay", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
-Ingredient.create(name:"Tumeric")
-Ingredient.create(name:"Anchovies")
-Ingredient.create(name:"Garlic")
 @a = User.create(name: "Ahamed", username: "ahamed1", password: "1234")
 User.create(name: "Daniel", username: "damndaniel", password: "password")
 Recipe.create(name: "Tumeric Caesar Salad", details: "Not good probably", user_id: @a.id)
