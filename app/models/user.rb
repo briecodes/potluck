c84d00eadb406f5981fd43ed9db8e86abac8ea14
briecodes <hello@brie.work>
2018-05-08 15:18:00 -0400

    fixed user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :user_events
   has_many :events, through: :user_events
-  has_many :user_recipes
-  has_many :recipes, through: :user_recipes
+  has_many :recipes
   
   validats :username, uniqueness: true
 end
