367afaf5ec6d9677211fb811ca71a98b8301d797
github username <jrgreiner115@gmail.com>
2018-05-08 10:05:00 -0400

    newform

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users, only: [:index, :create, :show, :edit, :update]
 
-  get '/login', to: 'sessions#new', as: "login"
-  get "/signup", to: "users#new", as: "signup"
+  get '/login', to: 'sessions#new', as: "log_in"
+  get "/signup", to: "users#new", as: "sign_up"
   post "/sessions", to: "sessions#create", as: "sessions"
-  post "/logout", to: "sessions#destroy", as: "logout"
+  post "/logout", to: "sessions#destroy", as: "log_out"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://gu vbides.rubyonrails.org/routing.html
 end
