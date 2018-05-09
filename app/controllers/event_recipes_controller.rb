dc043b9d3d1e5d42bd5f3b39b2f7ff6fd89cb163
briecodes <hello@brie.work>
2018-05-09 16:59:36 -0400

    eventrecipe successfully persists

--- app/controllers/event_recipes_controller.rb
+++ app/controllers/event_recipes_controller.rb
@@ -1,13 +1,17 @@
 class EventRecipesController < ApplicationController
     
     def new
-        byebug
-        EventRecipe.create(eventrecipe_params)
+        eventrecipe_params[:recipe_id].each do |r_id|
+            if r_id
+                EventRecipe.create(recipe_id: r_id, event_id: eventrecipe_params[:event_id])
+            end
+        end
+
         redirect_to event_path(params[:event_recipe][:event_id])
     end
 
     private
         def eventrecipe_params
-            params.require(:event_recipe).permit(:event_id, recipe_ids:[])
+            params.require(:event_recipe).permit(:event_id, recipe_id:[])
         end
 end
