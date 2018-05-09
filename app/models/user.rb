fff99a44aff2b9b7329cb73e38c2f1fc3e8056cc
github username <jrgreiner115@gmail.com>
2018-05-09 13:21:09 -0400

    user permissionings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :recipes
   has_many :user_events
   has_many :events, through: :user_events
 
   validates :username, uniqueness: true
   validates :username, format: { without: /\s/ }
+  validates :name, presence: true
 end
