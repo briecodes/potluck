7053e6b26e54d9b849037090915e59a1726e4588
briecodes <hello@brie.work>
2018-05-10 14:41:21 -0400

    seed cleaned up.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,27 +15,6 @@ csv.each do |row|
   t.save
 end
 
-
-# Event.destroy_all
-# User.destroy_all
-# Recipe.destroy_all
-
-#Events model testing seeds
-ahamed = User.create(name: "Ahamed", username: "ahamed1", password: "1234", password_confirmation: "1234")
-daniel = User.create(name: "Daniel", username: "damndaniel", password: "password", password_confirmation: "password")
-joe = User.create(name: "Joe", username: "joe", password: "5678", password_confirmation: "5678")
-brie = User.create(name: "Brie", username: "brie", password: "91011", password_confirmation: "91011")
-shun = User.create(name: "Shun", username: "theshun", password: "shunthis", password_confirmation: "shunthis")
-
-event1 = Event.create(title: "Mod1 Potluck", location: "Kay", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
-event2 = Event.create(title: "Mod2 Potluck", location: "Kay", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
-event3 = Event.create(title: "Mod3 Potluck", location: "Kay", date: "2018-01-07-12:00", details: "Let's celebrate!!!!")
-event4 = Event.create(title: "Mod4 Potluck", location: "Turing", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
-
-event1.users << ahamed << daniel << joe
-event2.users << ahamed << daniel << joe << brie << shun
-event3.users << ahamed << daniel << joe << brie
-
 usr_list = [
   {name: "Bilbo Baggins", username: "bilbo", password: "password"},
   {name: "Gollum", username: "gollum", password: "password"},
@@ -58,22 +37,22 @@ usr_list.each do |hash|
   User.create(hash)
 end
 
-10.times do
-  Event.create(title: Faker::Lovecraft.tome, date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today), location: Faker::RickAndMorty.location, details: Faker::RickAndMorty.quote)
-end
-
-
-# Recipe.create(name: "Tumeric Caesar Salad", details: "Not good probably", user_id: @a.id)
-# 20.times do
-#   r = Recipe.create(name: Faker::Food.dish, details: Faker::Food.description, user_id: rand(1..5))
-#   5.times do
-#     r.ingredients << Ingredient.create(name: Faker::Food.unique.ingredient)
-#   end
-# end
-
 20.times do
   r = Recipe.create(name: Faker::Food.dish, details: Faker::Food.description, user_id: rand(1..15))
   5.times do
     r.ingredients << Ingredient.create(name: Faker::Food.unique.ingredient)
   end
 end
+
+10.times do
+  Event.create(title: Faker::Lovecraft.tome, date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today), location: Faker::RickAndMorty.location, details: Faker::RickAndMorty.quote)
+end
+
+event1 = Event.create(title: "Mod1 Potluck", location: "Kay", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
+event2 = Event.create(title: "Mod2 Potluck", location: "Kay", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
+event3 = Event.create(title: "Mod3 Potluck", location: "Kay", date: "2018-01-07-12:00", details: "Let's celebrate!!!!")
+event4 = Event.create(title: "Mod4 Potluck", location: "Turing", date: "2018-05-07-12:00", details: "Let's celebrate!!!!")
+
+event1.users << ahamed << daniel << joe
+event2.users << ahamed << daniel << joe << brie << shun
+event3.users << ahamed << daniel << joe << brie
