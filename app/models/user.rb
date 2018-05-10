93dc10b5ace5d90fad645c039e2f7108e0e8ef2a
briecodes <hello@brie.work>
2018-05-10 12:32:39 -0400

    User model methods defined.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,28 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :username, format: { without: /\s/ }
   validates :name, presence: true
+
+  def self.all
+    self.all
+  end
+
+  def self.user_total
+    self.all.size
+  end
+
+  def self.user_most_recipes
+    @list = self.all.each do |a,b|
+      a.recipes.size <=> b.recipes.size
+    end
+  end
+
+  def self.user_recipe_average
+    Recipe.all.size / self.all.size
+  end
+
+  def self.social_butterfly
+    @popular_person = self.all.each do |a,b|
+      a.events.size <=> b.events.size
+    end.first
+  end
 end
