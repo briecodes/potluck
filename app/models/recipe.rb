2a31a01c46cf2b7ace9f6b73d9281683d400d2a8
Daniel Brazel <dbrazel4@gmail.com>
2018-05-10 13:53:38 -0400

    Recipe methods written

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,22 @@ class Recipe < ApplicationRecord
   has_many :events, through: :event_recipes
   validates :name, presence: true
   validates :details, presence: true
+
+  def self.popular
+    recipes_times_used = Hash.new
+    Recipe.all.each do |r|
+      recipes_times_used[r.name] = r.events.size
+    end
+    recipes_times_used = recipes_times_used.sort_by {|k, v| v}
+    recipes_times_used.to_a[-1][0]
+  end
+
+  def self.most_ingredients
+    number_of_ings = Hash.new
+    Recipe.all.each do |r|
+      number_of_ings[r.name] = r.ingredients.size
+    end
+    number_of_ings = number_of_ings.sort_by {|k, v| v}
+    number_of_ings.to_a[-1][0]
+  end
 end
