9f3c585facfb2a6db510700d403d87c5fbc230d3
github username <jrgreiner115@gmail.com>
2018-05-10 13:38:02 -0400

    some model methods & recipe show page format

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,15 @@ class Recipe < ApplicationRecord
   has_many :events, through: :event_recipes
   validates :name, presence: true
   validates :details, presence: true
+
+  def max_count(arr)
+    arr.uniq.map { |n| arr.count(n) }.max
+  end
+
+  def self.most_used_ingredient
+    @mosty = []
+    Recipe.all.map do |r|
+      @mosty << r.ingredients.ids
+    end
+  end
 end
